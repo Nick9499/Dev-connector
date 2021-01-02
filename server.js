1a1b395899c1f0b7c42466033854894ce283b5a9
Nick9499 <45527115+Nick9499@users.noreply.github.com>
2021-01-02 15:50:02 +0530

    express and mongo setup

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
-
+connectDB()
 app.get('/',(req,res)=> {
     res.send('API running...')
 })
+
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
+app.use('/api/auth',require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000
 
